f2fe401f217bb9fd5a7a09eaf7b5061fcb9387c2
shuklakumkum <shuklakumkum1710@gmail.com>
2026-01-01 20:08:49 +0530

    Update taskf3.py

--- taskF/taskf3.py
+++ taskF/taskf3.py
@@ -26,9 +26,9 @@ def get(item_id:int):
     return items[item_id]
 
 #delete item
-@taskf3.get("/items/{item_id}",status_code=status.HTTP_204_NO_CONTENT)
+@taskf3.delete("/items/{item_id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete(item_id:int):
     if item_id<0 or item_id>=len(items):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="item is not found")
     items.pop(item_id)
-    return
+    
